110c98e4d36dcc9a96c2e2b63f8c113c65639796
bdawg117 <brimandev@gmail.com>
2026-06-10 14:54:50 -0400

    Refactor laser data processing and enhance safe_wander logic for improved obstacle avoidance

--- src/aria_cpp/src/aria_wander.cpp
+++ src/aria_cpp/src/aria_wander.cpp
@@ -1,10 +1,12 @@
 #include <cmath>
+#include <format>
 #include <geometry_msgs/msg/twist.hpp>
 #include <iostream>
 #include <ranges>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <string>
+#include <algorithm>
 #include <vector>
 class ARIAWander : public rclcpp::Node {
    private:
@@ -73,15 +75,15 @@ void ARIAWander::laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg
 
 		for (size_t i = 0; i < ranges.size(); i++) {
 			if (std::isinf(ranges[i])) {
-				if (i > 0 && ranges[i - 1] >= (msg->range_max / 2)) {
-					sani_laser_data.push_back(msg->range_min);
-				} else {
-					sani_laser_data.push_back(msg->range_max);
-				}
+				sani_laser_data.push_back(msg->range_max);
 			} else {
 				sani_laser_data.push_back(ranges[i]);
 			}
 		}
+		// lidar is mounted 180° from robot forward, rotate so index 180 = forward
+		std::rotate(sani_laser_data.begin(),
+		            sani_laser_data.begin() + sani_laser_data.size() / 2,
+		            sani_laser_data.end());
 	}
 	safe_wander();
 }
@@ -123,13 +125,32 @@ void ARIAWander::safe_wander() {
 	float left_distance = 0;
 	float right_distance = 0;
 	float front_distance = 0;
-	int left_start = angleToIndex(-135 * M_PI / 180.0f);
-	int left_end = angleToIndex(-45 * M_PI / 180.0f);
-	int front_left_start = angleToIndex(-15 * M_PI / 180.0f);
+	int right_start = angleToIndex(-135 * M_PI / 180.0f);
+	int right_end = angleToIndex(-45 * M_PI / 180.0f);
+	int front_left_start = angleToIndex(-45 * M_PI / 180.0f);
 	int front_middle = angleToIndex(0 * M_PI / 180.0f);
-	int front_right_end = angleToIndex(15 * M_PI / 180.0f);
-	int right_start = angleToIndex(45 * M_PI / 180.0f);
-	int right_end = angleToIndex(135 * M_PI / 180.0f);
+	int front_right_end = angleToIndex(45 * M_PI / 180.0f);
+	int left_start = angleToIndex(45 * M_PI / 180.0f);
+	int left_end = angleToIndex(135 * M_PI / 180.0f);
+	int front_right_middle_whisker = angleToIndex(25 * M_PI / 180.0f);
+	int front_left_middle_whisker = angleToIndex(-25 * M_PI / 180.0f);
+
+	// constexpr int FRONT = 180;
+	// int right_start                = FRONT - 135;  // 45
+	// int right_end                  = FRONT - 45;   // 135
+	// int front_left_start           = FRONT - 45;   // 135
+	// int front_middle               = FRONT;         // 180
+	// int front_right_end            = FRONT + 45;   // 225
+	// int left_start                 = FRONT + 45;   // 225
+	// int left_end                   = FRONT + 135;  // 315
+	// int front_right_middle_whisker = FRONT + 25;   // 205
+	// int front_left_middle_whisker  = FRONT - 25;   // 155
+
+	int front_whiskers[] = {front_left_start, front_left_middle_whisker, front_middle, front_right_middle_whisker, front_right_end};
+	int small_shiskers[] = {front_left_middle_whisker, front_middle, front_right_middle_whisker};
+	float sw_dist[] = {sani_laser_data[front_left_middle_whisker], sani_laser_data[front_right_middle_whisker], sani_laser_data[front_middle]};
+
+
 
 	float right_average_distance = 0.0;
 	float left_average_distance = 0.0;
@@ -149,37 +170,71 @@ void ARIAWander::safe_wander() {
 		right_count += 1;
 	}
 
-	right_average_distance = right_distance / right_count;
-	left_average_distance = left_distance / left_count;
-
-	if (std::min(right_average_distance, left_average_distance) < 0.5) {
-		if (right_average_distance > left_average_distance) {
-			cmd.angular.z = -0.6 * (1 - left_average_distance);
-		} else {
-			cmd.angular.z = 0.6 * (1 - right_average_distance);
-		}
-	}
-
 	for (int i = front_left_start; i < front_right_end; i++) {
 		front_distance += sani_laser_data[i];
 		front_count += 1;
 	}
 
+	right_average_distance = right_distance / right_count;
+	left_average_distance = left_distance / left_count;
 	front_average_distance = front_distance / front_count;
+	float min_front_whisker = *std::min_element(std::begin(sw_dist),std::end(sw_dist));
 
-	if (front_average_distance < 1) {
-		cmd.linear.x = 0.7 * (1 - front_average_distance);
-		if (front_average_distance < 0.5) {
-			cmd.linear.x = -0.2;
-		}
-		if (left_average_distance > right_average_distance) {
-			cmd.angular.z = -1 * (1 - front_average_distance);
-		} else {
-			cmd.angular.z = 1 * (1 - front_average_distance);
-		}
-	} else {
+	if (min_front_whisker > 1) {
 		cmd.linear.x = 0.7;
+
+		if (2 > right_average_distance && right_average_distance < left_average_distance) {
+			cmd.angular.z = -1 * (1 - right_average_distance / 2);
+		} else if ((2 > left_average_distance && left_average_distance < right_average_distance)) {
+			cmd.angular.z = 1 * (1 - left_average_distance / 2);
+		}
+
+	} else if (min_front_whisker < 1) {
+		cmd.linear.x = 0.7 * min_front_whisker / 2 ;
+
+		if (right_average_distance > left_average_distance) {
+			cmd.angular.z = 0.8;
+		} else {
+			cmd.angular.z = -0.8;
+		}
+
 	}
+
+	RCLCPP_INFO_STREAM(this->get_logger(), "front(179): " << sani_laser_data[179]);
+
+
+	// RCLCPP_INFO_STREAM(this->get_logger(), "LEFT: " << sani_laser_data[front]);
+	// RCLCPP_INFO_STREAM(this->get_logger(), "RIGHT: " << sani_laser_data[back]);
+
+	// for (int whisker: front_whiskers) {
+	// 	RCLCPP_INFO_STREAM(this->get_logger(), sani_laser_data[whisker]);
+	// }
+
+
+
+
+	// if (front_average_distance < 1) {
+	// 	cmd.linear.x = 0.7 * (1 - front_average_distance);
+	// 	if (front_average_distance < 0.5) {
+	// 		cmd.linear.x = -0.2;
+	// 	}
+	// 	if (left_average_distance > right_average_distance) {
+	// 		cmd.angular.z = -1 * (1 - front_average_distance);
+	// 	} else if (left_average_distance < right_average_distance) {
+	// 		cmd.angular.z = 1 * (1 - front_average_distance);
+	// 	} else {
+	// 					cmd.angular.z = 1 * (1 - front_average_distance);
+	// 	}
+	// } else {
+	// 	cmd.linear.x = -0.7;
+	// 	if (std::min(right_average_distance, left_average_distance) < 0.5) {
+	// 		if (right_average_distance > left_average_distance) {
+	// 			cmd.angular.z = -0.6 * (1 - left_average_distance);
+	// 		} else {
+	// 			cmd.angular.z = 0.6 * (1 - right_average_distance);
+	// 		}
+	// 	}
+	// }
 
 	cmd_publisher_->publish(cmd);
 }
